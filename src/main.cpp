a867145732d8e28b630ec46f0379b3ba93be093a
JuanPRodriguezzz <161912514+JuanPRodriguezzz@users.noreply.github.com>
2025-10-20 15:02:58 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include "sistemahospital.h"
 
+/*
+ * MAIN MUY SIMPLE - Solo llama al método estático
+ * Todo el código está encapsulado en la clase SistemaHospital
+ */
+int main() {
+    // Punto de entrada - todo encapsulado en la clase
+    SistemaHospital::ejecutarAplicacion();
+    return 0;
+}
